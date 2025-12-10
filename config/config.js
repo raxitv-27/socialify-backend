7efb68a9a62a6ecd4ad411548a81317779f8970b
Raxit Vaghela <raxit.vaghela@intsof.com>
2025-12-10 14:54:08 +0530

    Fixed docker file and server.js file for fixing ci/cd

--- config/config.js
+++ config/config.js
@@ -26,6 +26,9 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || "postgres",
+    dialectOptions: {
+      socketPath: process.env.DB_HOST,
+    },
     logging: false,
   },
 };
